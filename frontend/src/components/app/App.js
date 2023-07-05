e4add52f78b1af718376953c18e9c0df905924ee
Julian Festing <julfesting@gmail.com>
2023-07-05 10:51:55 +0100

    setup react frontend with signup path

---
+++ frontend/src/components/app/App.js
@@ -0,0 +1,21 @@
+import logo from '../../logo.svg';
+import './App.css';
+import SignUpForm from '../signupForm/SignupForm'
+import React, { useState } from 'react';
+import {
+  useNavigate,
+  Routes,
+  Route
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/signup' element={<SignUpForm navigate={ useNavigate() }/>}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/signupForm/SignupForm.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const SignupForm = () => {
+
+  return (
+    <div className="signUpContainer">
+      
+    </div>
+  );
+}
+
+export default SignupForm;
