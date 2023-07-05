5b7f16661e8557843abbb976b5efad5588ca9517
Julian Festing <julfesting@gmail.com>
2023-07-05 13:09:02 +0100

    created signup form to test connection of frontend tobackend database

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,7 +1,6 @@
-import logo from '../../logo.svg';
 import './App.css';
 import SignUpForm from '../signupForm/SignupForm'
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import {
   useNavigate,
   Routes,
