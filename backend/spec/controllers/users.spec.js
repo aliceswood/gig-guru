0d343bc33bc9029d5604015aec887af6227d9830
Julian Festing <julfesting@gmail.com>
2023-07-07 16:51:22 +0100

    added second test database and configured server 9999 to active for test. also remodelled the connections in db.js and mdb_helper to account for this. updated tests and added new script that sets env

--- backend/spec/controllers/users.spec.js
+++ backend/spec/controllers/users.spec.js
@@ -13,18 +13,18 @@ describe("/users", () => {
     test("the response code is 201", async () => {
       let response = await request(app)
         .post("/users")
-        .send({email: "test@example.com", password: "1234", username: "test1"})
+        .send({email: "users@exampletest.com", password: "1234", username: "user1"})
       expect(response.statusCode).toBe(201)
     });
 
     test("a user is created", async () => {
       await request(app)
         .post("/users")
-        .send({email: "test@example.com", password: "1234", username: "test1"})
+        .send({email: "users@exampletest.com", password: "1234", username: "user1"})
       let users = await User.find()
       let newUser = users[users.length - 1]
-      expect(newUser.email).toEqual("test@example.com")
-      expect(newUser.username).toEqual("test1")
+      expect(newUser.email).toEqual("users@exampletest.com")
+      expect(newUser.username).toEqual("user1")
     });
   });
 })
--- backend/spec/mongodb_helper.js
+++ backend/spec/mongodb_helper.js
@@ -1,5 +1,19 @@
-var mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const config = require('config');
+const { MONGODBURI } = config.get('test');
 
-afterAll( async () => {
-  await mongoose.disconnect();
-});
+console.log('hello')
+
+const connectTestDB = async () => {
+  try {
+    await mongoose.connect(MONGODBURI, {
+      useNewUrlParser: true
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectTestDB;
