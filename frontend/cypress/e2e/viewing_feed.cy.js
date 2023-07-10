97e4755526c18fb342368426d4bb53bbe3d754b2
Barney Wilton <B.Wilton1993@gmail.com>
2023-07-10 12:29:55 +0100

    Cleaned up e2e tests for feed.js

--- frontend/cypress/e2e/viewing_feed.cy.js
+++ frontend/cypress/e2e/viewing_feed.cy.js
@@ -1,17 +1,10 @@
-// To be updated when liking an event is tested and implemented
-
-// import Feed from "../../src/components/feed/Feed";
-// import { getDate } from "../../src/components/feed/Feed";
-
 describe("Viewing Feed", () => {
   
   beforeEach(() => {
     cy.signup("user@email.com", "12345678", "username");
-    cy.clock(new Date(), ["Date"]);
   });
   
   it("with valid credentials, redirects to '/'", () => {
-    const date = cy.getDate();
     cy.visit("/login");
     cy.get("#email").type("user@email.com");
     cy.get("#password").type("12345678");
