f4bf36423dfb1a2c0d153e0b3464f515882cd127
Charlie Parker <cshjp@MASTUDENT-Charlie-Parker-C02G4FQVQ6L4.local>
2023-07-10 13:41:06 +0100

    added the api requests to the city that you choose

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -5,17 +5,25 @@ import city_names from '../cities/Cities'
 const Feed = ({ navigate }) => {
   const [data, setData] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [selectedCity, setSelectedCity] = useState('');
 
   var date = new Date();
   date = date.toISOString().split('T')[0];
   date += 'T00:00:00Z';
 
   useEffect(() => {
-    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationId=KZFzniwnSyZfZ7v7nJ&city=london&size=20&sort=date,asc&startDateTime=${date}&apikey=JtjU0ATGKIgSLhSEz5UQnr1LFy9hYZ0s`)
+    if (selectedCity !== "") {
+    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationId=KZFzniwnSyZfZ7v7nJ&city=${selectedCity}&size=5&sort=date,asc&startDateTime=${date}&apikey=JtjU0ATGKIgSLhSEz5UQnr1LFy9hYZ0s`)
       .then(response => response.json())
       .then(json => setData(json._embedded.events))
       .catch(error => console.error(error));
-  }, []);
+    } else {
+      fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationId=KZFzniwnSyZfZ7v7nJ&city=london&size=5&sort=date,asc&startDateTime=${date}&apikey=JtjU0ATGKIgSLhSEz5UQnr1LFy9hYZ0s`)
+      .then(response => response.json())
+      .then(json => setData(json._embedded.events))
+      .catch(error => console.error(error));
+    }
+  }, [selectedCity]);
 
   const logout = () => {
     window.localStorage.removeItem("token")
@@ -37,7 +45,7 @@ const Feed = ({ navigate }) => {
         <div>
           <button type="button" id="logout" onClick={logout}>Logout</button>
           <label for="city-selector">Choose a location:</label>
-          <input list="cities" id="city-selector" name="city-selector" />
+          <input list="cities" id="city-selector" name="city-selector" value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)} />
 
           <datalist id="cities">
             {city_names.map(city => <option value={city}></option>)}
