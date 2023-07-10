c328a4de0d7fca2e7f6288f0a6d06382091115cd
Ormeline <ormelineluyengi@outlook.com>
2023-07-10 15:41:43 +0100

    event-page

---
+++ frontend/src/components/event-page/EventPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ViewEvent = () => {
+    return (
+        <>
+        <div>
+            <h1> I am event</h1></div>
+      </>
+      )
+    }
+ export default ViewEvent;
