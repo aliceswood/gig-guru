61b3a4b97c0f0c6feb90f9c708d8f0fde37aebd2
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2023-07-11 16:31:35 +0100

    Update users.js
    removing commented code

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,17 +2,6 @@ const User = require("../models/user");
 const tokenDecoder = require("../models/token_decoder");
 
 const UsersController = {
-  // Create: (req, res) => {
-  //   const user = new User(req.body);
-  //   user.save((err) => {
-  //     if (err) {
-  //       res.status(400).json({message: 'Bad request'})
-  //     } else {
-  //       res.status(201).json({ message: 'OK' });
-  //     }
-  //   });
-  // },
-
   Create: (req, res) => {
     const checkEmail = req.body.email;
     
