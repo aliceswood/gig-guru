3231f8e775e84750ecd5d5c6c3ec1a1ecb46fe0c
Alice Wood <alicesophiewood@gmail.com>
2023-07-12 18:43:05 +0100

    commenting out event info icons (calendar and location image

--- frontend/src/components/eventInfo/EventInfo.js
+++ frontend/src/components/eventInfo/EventInfo.js
@@ -1,29 +1,42 @@
 import React from "react";
-import './EventInfo.css'
+import "./EventInfo.css";
 import CustomControlsMap from "../map/Map";
 
 const EventInfo = (props) => {
 
-  // test not functional, probably need token and 
-
   return (
     <>
       <div className="event-info">
-        <img id="image" data-cy="event-image" src={props.images[3].url} alt="event"></img>
-        <div id="event-title" data-cy="event-name">{props.name}</div>
-        <div id="event-time">
-          <div data-cy="event-date">Date: {props.dates.start.localDate}</div>
-          <div data-cy="event-time">Time: {props.dates.start.localTime}</div>
-         </div>
-         <div id="venue-details"> 
-          <div data-cy="event-venue">{props._embedded.venues[0].name}</div>
-          <div data-cy="event-postalcode">{props._embedded.venues[0].postalCode}</div>
+        <img
+          id="image"
+          data-cy="event-image"
+          src={props.images[3].url}
+          alt="event"
+        ></img>
+        <div id="event-title" data-cy="event-name">
+          {props.name}
         </div>
-        <div id='map'>
+        {/* <div id="time-container">
+          <img className="calendar" src="calendar.svg" alt="calendar" /> */}
+          <div id="event-time">
+            <div data-cy="event-date">Date: {props.dates.start.localDate}</div>
+            <div data-cy="event-time">Time: {props.dates.start.localTime}</div>
+          </div>
+        {/* </div> */}
+        {/* <div id="venue-container">
+          <img className="location" src="location.jpeg" alt="location" /> */}
+          <div id="venue-details">
+            <div data-cy="event-venue">{props._embedded.venues[0].name}</div>
+            <div data-cy="event-postalcode">
+              {props._embedded.venues[0].postalCode}
+            </div>
+          </div>
+        {/* </div> */}
+        <div id="map">
           <CustomControlsMap />
         </div>
       </div>
     </>
-    );
-  };
+  );
+};
 export default EventInfo;
