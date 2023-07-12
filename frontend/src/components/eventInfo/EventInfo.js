652d6c734f3699474626bd865d5cd2a525005757
Alice Wood <alicesophiewood@gmail.com>
2023-07-12 17:38:15 +0100

    amending event page to image 3 for better quality

--- frontend/src/components/eventInfo/EventInfo.js
+++ frontend/src/components/eventInfo/EventInfo.js
@@ -9,7 +9,7 @@ const EventInfo = (props) => {
   return (
     <>
       <div className="event-info">
-        <img id="image" data-cy="event-image" src={props.images[0].url} alt="event"></img>
+        <img id="image" data-cy="event-image" src={props.images[3].url} alt="event"></img>
         <div id="event-title" data-cy="event-name">{props.name}</div>
         <div id="event-time">
           <div data-cy="event-date">Date: {props.dates.start.localDate}</div>
