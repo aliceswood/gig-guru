d52856ad39edfd21f1c1d41d399d39299eae6b18
Alice Wood <alicesophiewood@gmail.com>
2023-07-13 12:10:48 +0100

    wave now on the log in and sign up page

--- frontend/src/components/loginForm/LoginForm.js
+++ frontend/src/components/loginForm/LoginForm.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import "./LoginForm.css";
+import WaveSvg from "../event-page/Wave.svg"
+
 
 const LogInForm = ({ navigate }) => {
   const [email, setEmail] = useState("");
@@ -50,16 +52,18 @@ const LogInForm = ({ navigate }) => {
 
     return (
       <>
-        <div className='LogInContainer'>
-          <form className="logInForm" onSubmit={handleSubmit}>
-          <h1 className='logInHeading'>Log in</h1>
-            <input className="formInput" placeholder='Email' id="email" type='text' value={ email } onChange={handleEmailChange} />
-            <input className="formInput" placeholder='Password' id="password" type='password' value={ password } onChange={handlePasswordChange} />
-            <input className="button" id='submit' type="submit" value="Submit" />
-          <div>
-            <button className="redirectButton" type="button" id="signup" onClick={signup}>Don't have an account?</button>
+        <div className="login-page" style={{ backgroundImage: `url(${WaveSvg})`, height: '100vh'}}>
+          <div className='LogInContainer'>
+            <form className="logInForm" onSubmit={handleSubmit}>
+            <h1 className='logInHeading'>Log in</h1>
+              <input className="formInput" placeholder='Email' id="email" type='text' value={ email } onChange={handleEmailChange} />
+              <input className="formInput" placeholder='Password' id="password" type='password' value={ password } onChange={handlePasswordChange} />
+              <input className="button" id='submit' type="submit" value="Submit" />
+            <div>
+              <button className="redirectButton" type="button" id="signup" onClick={signup}>Don't have an account?</button>
+            </div>
+            </form>
           </div>
-          </form>
         </div>
       </>
     );
--- frontend/src/components/signupForm/SignupForm.js
+++ frontend/src/components/signupForm/SignupForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './SignupForm.css'
+import WaveSvg from "../event-page/Wave.svg"
+
 
 const SignupForm = ({ navigate }) => {
   const [email, setEmail] = useState('')
@@ -44,18 +46,22 @@ const SignupForm = ({ navigate }) => {
   }
 
   return (
-    <div className='signUpContainer'>
-        <form className='signUpForm' onSubmit={handleSubmit}>
-          <h1 className='signUpHeading'>Welcome to Gig Guru</h1>
-          <input className="formInput" placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
-          <input className="formInput" placeholder="Username" id="username" type='text' value={ username } onChange={handleUsernameChange} />
-          <input className="formInput" placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
-          <input className="button" id='submit' type="submit" value="Submit" />
-          <div>
-            <button className="redirectButton" type="button" id="login" onClick={login}>Already have an account?</button>
-          </div>
-        </form>
-    </div>
+    <>
+      <div className="signup-page" style={{ backgroundImage: `url(${WaveSvg})`, height: '100vh'}}>
+        <div className='signUpContainer'>
+            <form className='signUpForm' onSubmit={handleSubmit}>
+              <h1 className='signUpHeading'>Welcome to Gig Guru</h1>
+              <input className="formInput" placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
+              <input className="formInput" placeholder="Username" id="username" type='text' value={ username } onChange={handleUsernameChange} />
+              <input className="formInput" placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
+              <input className="button" id='submit' type="submit" value="Submit" />
+              <div>
+                <button className="redirectButton" type="button" id="login" onClick={login}>Already have an account?</button>
+              </div>
+            </form>
+        </div>
+      </div>
+    </>
   );
 }
 
